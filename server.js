ee967a4f1ea1f677cf03aa5d3c5c88fb7aa36727
Alysha Gilliard <alyshargilliard@gmail.com>
2022-03-16 21:04:47 -0400

    Updated linting for initial files

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+
 const app = express();
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+  console.log(`Listening on port ${port}`);
 });
