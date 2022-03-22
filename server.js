d8ca2e58dd7bdb6b881ad5a03b7c593669020c13
Alysha Gilliard <alyshargilliard@gmail.com>
2022-03-21 22:00:59 -0400

    Starting initilization of db

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const postgres = require('postgres');
 
 const app = express();
 const port = 3000;
+
+const db = postgres({
+  host: '127.0.0.1',
+  database: 'products-service',
+  username: 'user',
+  password: 'password',
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
