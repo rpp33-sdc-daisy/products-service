55e75e037e82ac8000337fc93d4839fd0cfa06b6
Alysha Gilliard <alyshargilliard@gmail.com>
2022-04-04 18:26:27 -0400

    Initial skeleton for tests

--- server.test.js
+++ server.test.js
@@ -1,13 +1,21 @@
 const request = require('supertest');
 const app = require('./server.js');
 
-describe('GET /products', () => {
-  test('GET /products return 200', async () => {
+describe('Products API Unit Tests', () => {
+  test('GET /products retrieves a list of products', async () => {
     await request(app)
       .get('/products')
       .expect(200)
       .end((err, res) => {
         console.log(err);
       });
+  });
+
+  test('GET /products/:product_id returns all product level information for a specified product id', async () => {
+
+  });
+
+  test('GET /products/:product_id/styles returns the all styles available for the given product', async () => {
+
   });
 });
