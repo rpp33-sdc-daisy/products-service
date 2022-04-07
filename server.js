9a082798d7a566e177a24067b122ec8db12bf290
Alysha Gilliard <alyshargilliard@gmail.com>
2022-04-06 22:40:30 -0400

    Set query params

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Client } = require('pg');
 
 const {
-  getAllProducts, getProduct, getStyles, getAllFeatures,
+  getProduct, getStyles, getAllFeatures,
 } = require('./queries.js');
 
 // http://127.0.0.1:8080/
@@ -20,6 +20,9 @@ client.connect();
 
 // GET /products Retrieves the list of products.
 app.get('/products', (req, res) => {
+  const count = req.query.count || 5;
+  const page = (req.query.page || 0) * 20;
+  const getAllProducts = `SELECT products.*, json_agg(json_build_object('id', features.id, 'feature', features.feature, 'value', features.value)) AS features from products JOIN features ON products.product_id=features.product_id GROUP BY products.product_id LIMIT ${count} OFFSET ${page};`;
   client.query(getAllProducts)
     .then((response) => {
       const products = response.rows;
