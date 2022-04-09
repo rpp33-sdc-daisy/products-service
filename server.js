6c1eefccadc5c9142ab555e963f2d4abf33a6d8e
Alysha Gilliard <alyshargilliard@gmail.com>
2022-04-09 15:27:36 -0400

    Complete getProducts route

--- server.js
+++ server.js
@@ -57,53 +57,6 @@ app.get('/products/:product_id', (req, res) => {
     });
 });
 
-// GET /products/:product_id/styles returns the all styles available for the given product.
-app.get('/products/:product_id/styles', (req, res) => {
-  const productId = req.params.product_id;
-  const getStyles = `SELECT products.id as product_id,
-    json_agg(
-      json_build_object(
-        'style_id', styles.id, 'name', styles.name, 'original_price', styles.original_price, 'sale_price', styles.sale_price, 'default?', styles.default_style, 'photos', (
-          SELECT
-            json_agg(
-              json_build_object(
-                'thumbnail_url', photos.thumbnail_url,
-                'url', photos.url
-              )
-            )
-          FROM photos WHERE styles.id=photos.style_id
-        ),
-        'skus', (
-          SELECT
-          json_object_agg(skus.id,
-            json_build_object(
-              'quantity', skus.quantity,
-              'size', skus.size
-            )
-          )
-          FROM skus WHERE styles.id=skus.style_id
-        )
-      )
-    )
-    AS results FROM products
-    LEFT JOIN styles ON products.id=styles.product_id
-    WHERE products.id=${productId}
-    GROUP BY products.id;`;
-
-  client.query(getStyles)
-    .then((response) => {
-      if (response.rows.length === 0) throw new Error('Styles not found', { cause: 'Styles not found' });
-      res.send(response.rows[0]);
-    })
-    .catch((err) => {
-      if (err.cause === 'Styles not found') {
-        res.status(404).send('Styles not found: Please enter a different product id');
-      } else {
-        res.status(400).send('Error: Please try your request again.');
-      }
-    });
-});
-
 const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
