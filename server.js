12329124fbc98731b893f5651c8c86b53dbaabc7
Alysha Gilliard <alyshargilliard@gmail.com>
2022-04-27 22:18:52 -0400

    Updated redis config

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/products', (req, res) => {
   const page = (req.query.page || 0) * count;
   const getAllProducts = `SELECT * FROM products LIMIT ${count} OFFSET ${page};`;
   const redisClient = createClient({
-    url: `redis://default:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
   });
   pool.connect()
     .then((client) => {
