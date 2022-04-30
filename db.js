26f4187816564997ac0f78b8f5af887d5c336cb6
Alysha Gilliard <alyshargilliard@gmail.com>
2022-04-30 11:14:49 -0400

    Updated config for pg db

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
+  user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: 'products-service',
   password: process.env.DB_PASSWORD,
